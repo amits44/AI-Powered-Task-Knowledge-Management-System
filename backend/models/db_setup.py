b2937fc89460fe2e498475fc348ae04e8fd02a7f
Amit <amitshantijyala2004@gmail.com>
2026-05-30 14:18:44 +0530

    Initial commit for backend setup

--- backend/models/db_setup.py
+++ backend/models/db_setup.py
@@ -15,7 +15,6 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
